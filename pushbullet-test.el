d7f6d20bfdf06591585fb3f1928cab22ac0d8063
Abhishek L <abhishekl.2006@gmail.com>
2014-02-02 10:23:36 +0530

    Whitespace cleanup
    * pushbullet-test.el: TABs in strings

--- pushbullet-test.el
+++ pushbullet-test.el
@@ -1,38 +1,36 @@
 (require 'ert)
 (require 'pushbullet)
 
-(ert-deftest pb/json-extract ()
-  (let ((devices-json "	{
-			\"devices\":[
-				   {
-				   \"id\":19137,
-				   \"extras\":{
-				   \"manufacturer\":\"samsung\",
-				   \"model\":\"Galaxy Nexus\",
-				   \"android_version\":\"4.1.1\",
-				   \"sdk_version\":\"16\",
-				   \"app_version\":\"8\",
-				   \"nickname\":\"Galaxy Nexus\"
-				   }
-				   }
-				   ],
-			\"shared_devices\":[
-					  {
-					  \"id\":7,
-					  \"extras\":{
-					  \"manufacturer\":\"samsung\",
-					  \"model\":\"Galaxy Nexus\",
-					  \"android_version\":\"4.1.1\",
-					  \"sdk_version\":\"16\",
-					  \"app_version\":\"8\"
-					  },
-					  \"owner_name\":\"Alex\"
-					  }
-					  ]
-			}"))
-    (should (equal '(19137) (pb/json-extract 'id 'devices devices-json)))
-    (should (equal '((devices . (19137))
-		     (shared  . (7)))
-		   (pb/extract-devices-all devices-json))
-	    )))
 
+(ert-deftest pb/json-extract ()
+  (let ((json-object-type 'alist)
+        (devices-json "{
+                        \"devices\":[
+                                   {
+                                   \"iden\":19137,
+                                   \"extras\":{
+                                   \"manufacturer\":\"samsung\",
+                                   \"model\":\"Galaxy Nexus\",
+                                   \"android_version\":\"4.1.1\",
+                                   \"sdk_version\":\"16\",
+                                          \"app_version\":\"8\",
+                                         \"nickname\":\"Galaxy Nexus\"
+                                   }
+                                   }
+                                   ],
+                        \"shared_devices\":[
+                                          {
+                                          \"iden\":7,
+                                          \"extras\":{
+                                          \"manufacturer\":\"samsung\",
+                                          \"model\":\"Galaxy Nexus\",
+                                          \"android_version\":\"4.1.1\",
+                                          \"sdk_version\":\"16\",
+                                          \"app_version\":\"8\"
+                                          },
+                                          \"owner_name\":\"Alex\"
+                                          }
+                                          ]
+                        }"))
+    (should (equal '(19137) (pb/json-extract 'iden 'devices devices-json)))
+    (should (equal '(7) (pb/json-extract 'iden 'shared_devices devices-json)))))

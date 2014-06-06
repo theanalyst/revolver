578499a469ce1abab3f4411aa8cdb787b032150d
Abhishek L <abhishek.lekshmanan@gmail.com>
2014-06-07 01:47:48 +0530

    update tests with updated functions
    Make ert tests aware of updated functions and data structures

--- pushbullet-test.el
+++ pushbullet-test.el
@@ -1,7 +1,6 @@
 (require 'ert)
 (require 'pushbullet)
 
-
 (ert-deftest pb/json-extract ()
   (let ((json-object-type 'alist)
         (devices-json "{
@@ -17,25 +16,11 @@
                                          \"nickname\":\"Galaxy Nexus\"
                                    }
                                    }
-                                   ],
-                        \"shared_devices\":[
-                                          {
-                                          \"iden\":7,
-                                          \"extras\":{
-                                          \"manufacturer\":\"samsung\",
-                                          \"model\":\"Galaxy Nexus\",
-                                          \"android_version\":\"4.1.1\",
-                                          \"sdk_version\":\"16\",
-                                          \"app_version\":\"8\"
-                                          },
-                                          \"owner_name\":\"Alex\"
-                                          }
-                                          ]
+                                   ]
                         }"))
-    (should (equal '(19137) (pb/json-extract 'iden 'devices devices-json)))
-    (should (equal '(7) (pb/json-extract 'iden 'shared_devices devices-json)))))
+    (should (equal '(19137) (pb/json-extract 'iden 'devices devices-json)))))
 
 (ert-deftest pushbullet-clear-devices ()
-  (setq pb/device-id-table (ht ("devices" 123)))
-  (pb/clear-devices)
-  (should (equal nil (ht-items pb/device-id-table))))
+  (setq pb/device-ids '(123))
+  (pushbullet-clear-devices)
+  (should (equal nil  pb/device-ids)))
